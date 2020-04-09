d48bbc461d6d3f4a0cee77acd577733c97254dc7
James Adams <james@jmablog.com>
2020-04-09 17:31:13 +0100

    Correct menu items entered

--- app.js
+++ app.js
@@ -1,103 +1,122 @@
 let currentModal;
 let id;
-var span = document.getElementsByClassName("close");
+var span = document.getElementsByClassName('close');
 
-// When the user clicks the button, open the modal 
-document.getElementById("btnStim1").onclick = function() {
-    currentModal = document.getElementById("modalStim1");
-    currentModal.style.display = "block";
-}
+// When the user clicks the button, open the modal
+document.getElementById('btnStim1').onclick = function() {
+	currentModal = document.getElementById('modalStim1');
+	currentModal.style.display = 'block';
 
-document.getElementById("btnStim2").onclick = function() {
-    currentModal = document.getElementById("modalStim2");
-    currentModal.style.display = "block";
-}
+	reset_form();
+};
 
-document.getElementById("btnUltrasound").onclick = function() {
-    currentModal = document.getElementById("modalUltrasound");
-    currentModal.style.display = "block";
-}
+document.getElementById('btnStim2').onclick = function() {
+	currentModal = document.getElementById('modalStim2');
+	currentModal.style.display = 'block';
 
-document.getElementById("btnTENS").onclick = function() {
-    currentModal = document.getElementById("modalTENS");
-    currentModal.style.display = "block";
-}
+	reset_form();
+};
+
+document.getElementById('btnUltrasound').onclick = function() {
+	currentModal = document.getElementById('modalUltrasound');
+	currentModal.style.display = 'block';
+
+	reset_form();
+};
+
+document.getElementById('btnTENS').onclick = function() {
+	currentModal = document.getElementById('modalTENS');
+	currentModal.style.display = 'block';
+
+	reset_form();
+};
 
 // When the user clicks on <span> (x), close the modal
-document.getElementById("modalStim1Close").onclick = function() {
-    // currentModal.style.display = "none";
-    // currentModal.getElementsByClassName("choices")[0].style.display = "block";
-    // currentModal.getElementsByClassName("summary")[0].style.display = "none";
-    reset_page();
-}
+document.getElementById('modalStim1Close').onclick = function() {
+	// currentModal.style.display = "none";
+	// currentModal.getElementsByClassName("choices")[0].style.display = "block";
+	// currentModal.getElementsByClassName("summary")[0].style.display = "none";
+	reset_page();
+};
 
-document.getElementById("modalStim2Close").onclick = function() {
-    reset_page();
-}
+document.getElementById('modalStim2Close').onclick = function() {
+	reset_page();
+};
 
-document.getElementById("modalUltrasoundClose").onclick = function() {
-    reset_page();
-}
+document.getElementById('modalUltrasoundClose').onclick = function() {
+	reset_page();
+};
 
-document.getElementById("modalTENSClose").onclick = function() {
-    reset_page();
-}
+document.getElementById('modalTENSClose').onclick = function() {
+	reset_page();
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == currentModal) {
-    // currentModal.style.display = "none";
-    // currentModal.getElementsByClassName("choices")[0].style.display = "block";
-    // currentModal.getElementsByClassName("summary")[0].style.display = "none";
-    reset_page();
-  }
-}
+	if (event.target == currentModal) {
+		// currentModal.style.display = "none";
+		// currentModal.getElementsByClassName("choices")[0].style.display = "block";
+		// currentModal.getElementsByClassName("summary")[0].style.display = "none";
+		reset_page();
+	}
+};
 
 function reset_page() {
-    currentModal.style.display = "none";
-    currentModal.getElementsByClassName("choices")[0].style.display = "block";
-    currentModal.getElementsByClassName("summary")[0].style.display = "none";
+	currentModal.style.display = 'none';
+	currentModal.getElementsByClassName('choices')[0].style.display = 'block';
+	currentModal.getElementsByClassName('summary')[0].style.display = 'none';
 
-    reset_form();
-    
+	reset_form();
 }
 
 function reset_form() {
-    currentModal.getElementsByTagName("form")[0].reset();
-    rangesToReset = currentModal.getElementsByClassName("range-display");
-        for (i = 0; i<rangesToReset.length; i++) {
-            rangesToReset[i].innerHTML = '0';
-        }
+	currentModal.getElementsByTagName('form')[0].reset();
+	rangesToReset = currentModal.getElementsByClassName('range-display');
+	for (i = 0; i < rangesToReset.length; i++) {
+		rangesToReset[i].innerHTML = '0';
+	}
+
+	document.getElementById('ultrasound-intensity-units').innerHTML = 'W/cm2';
+	document.getElementById('ultrasound-intensity-units-sum').innerHTML = 'W/cm2';
 }
 
 function showSummary() {
-    currentModal.getElementsByClassName("choices")[0].style.display = "none";
-    currentModal.getElementsByClassName("summary")[0].style.display = "block";
+	currentModal.getElementsByClassName('choices')[0].style.display = 'none';
+	currentModal.getElementsByClassName('summary')[0].style.display = 'block';
 
-    var selectEntries = currentModal.getElementsByClassName("select-entry");
-    var selectResults = currentModal.getElementsByClassName("select-result");
+	var selectEntries = currentModal.getElementsByClassName('select-entry');
+	var selectResults = currentModal.getElementsByClassName('select-result');
 
-    for (i = 0; i < selectResults.length; i++){
-        selectResults[i].innerHTML = selectEntries[i].options[selectEntries[i].selectedIndex].text;
-    }
+	for (i = 0; i < selectResults.length; i++) {
+		selectResults[i].innerHTML = selectEntries[i].options[selectEntries[i].selectedIndex].text;
+	}
 
-    var valueEntries = currentModal.getElementsByClassName("value-entry");
-    var valueResults = currentModal.getElementsByClassName("value-result");
+	var valueEntries = currentModal.getElementsByClassName('value-entry');
+	var valueResults = currentModal.getElementsByClassName('value-result');
 
-    for (i = 0; i < valueResults.length; i++){
-        valueResults[i].innerHTML = valueEntries[i].value;
-    }
+	for (i = 0; i < valueResults.length; i++) {
+		valueResults[i].innerHTML = valueEntries[i].value;
+	}
 
-    // var textareaEntries = currentModal.getElementsByClassName("textarea-entry");
-    // var textareaResults = currentModal.getElementsByClassName("textarea-result");
+	// var textareaEntries = currentModal.getElementsByClassName("textarea-entry");
+	// var textareaResults = currentModal.getElementsByClassName("textarea-result");
 
-    // for (i = 0; i < textareaResults.length; i++){
-    //     textareaResults[i].innerHTML = textareaEntries[i].value;
-    // }
+	// for (i = 0; i < textareaResults.length; i++){
+	//     textareaResults[i].innerHTML = textareaEntries[i].value;
+	// }
 }
 
-function updateRange() {
-    document.getElementById("stim2-range-display").innerHTML = document.getElementById("stim2-range").value;
+// function updateRange() {
+// 	document.getElementById('stim2-range-display').innerHTML = document.getElementById('stim2-range').value;
+// }
+
+// document.getElementById('stim2-range').addEventListener('input', updateRange);
+
+function updateIntensityUnits() {
+	intensity = document.getElementById('ultrasound-display');
+	document.getElementById('ultrasound-intensity-units').innerHTML = intensity.options[intensity.selectedIndex].text;
+	document.getElementById('ultrasound-intensity-units-sum').innerHTML =
+		intensity.options[intensity.selectedIndex].text;
 }
 
-document.getElementById("stim2-range").addEventListener("input", updateRange);
+document.getElementById('ultrasound-display').addEventListener('input', updateIntensityUnits);
